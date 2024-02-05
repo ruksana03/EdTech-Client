086e367f87fb933d6118def17b27e65a18468138
nayon117 <hasibul.nayon1@gmail.com>
2024-02-05 22:42:26 +0600

    add add course route

---
+++ src/page/dashboard/Teacher/AddCourse.jsx
@@ -0,0 +1,152 @@
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { useSelector } from "react-redux";
+import { TbFidgetSpinner } from "react-icons/tb";
+import { useState } from "react";
+
+const image_hosting_key = import.meta.env.VITE_IMGBB_API_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
+const AddCourse = () => {
+  const [loading, setLoading] = useState(false);
+  const { register, handleSubmit, reset } = useForm();
+  const user = useSelector((state) => state.data.user.user);
+  const axiosPublic = useAxiosPublic();
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+
+      const imageFile = { image: data.image[0] };
+      const res = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
+
+      if (res.data.success) {
+        const courseItem = {
+          title: data.title,
+          name: user?.name,
+          email: user?.email,
+          price: parseFloat(data.price),
+          category: data.category,
+          details: data.details,
+          image: res.data.data.display_url,
+        };
+
+        console.log(courseItem);
+
+        const { data: datas = [] } = await axiosPublic.post(
+          "/courses ",
+          courseItem
+        );
+
+        if (datas) {
+          reset();
+          toast.success("Your class is pending! Wait for admin approval");
+        }
+      }
+    } catch (error) {
+      // Handle error, show toast, etc.
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="mx-auto max-w-3xl p-4 mt-16 bg-white rounded-md shadow-md">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Title */}
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Title*
+          </label>
+          <input
+            {...register("title", { required: true })}
+            type="text"
+            placeholder="Enter the title..."
+            className="input w-full"
+          />
+        </div>
+
+        {/* Price and Category */}
+        <div className="flex items-center gap-6">
+          {/* Price */}
+          <div className="flex-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Price*
+            </label>
+            <input
+              {...register("price", { required: true })}
+              type="number"
+              placeholder="Enter the price"
+              className="input w-full"
+            />
+          </div>
+
+          {/* Category */}
+          <div className="flex-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Category*
+            </label>
+            <select
+              {...register("category", { required: true })}
+              className="select w-full"
+            >
+              <option value="" disabled>
+                Select a category
+              </option>
+              <option value="popular">Popular</option>
+              <option value="law">Law</option>
+              <option value="physics">Physics</option>
+              <option value="chemistry">Chemistry</option>
+              <option value="programming">Programming</option>
+              <option value="engineering">Engineering</option>
+            </select>
+          </div>
+        </div>
+
+        {/* Details */}
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Details
+          </label>
+          <textarea
+            {...register("details", { required: true })}
+            className="textarea h-16 w-full"
+            placeholder="Enter details..."
+          ></textarea>
+        </div>
+
+        {/* Image Upload */}
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Image
+          </label>
+          <input
+            {...register("image", { required: true })}
+            type="file"
+            className="file-input w-full text-first"
+          />
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="btn text-white bg-gradient-to-r from-second to-first focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full"
+          disabled={loading}
+        >
+          {loading ? (
+            <TbFidgetSpinner className="animate-spin text-white m-auto" />
+          ) : (
+            "Add Course"
+          )}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddCourse;
