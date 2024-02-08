efb422ce37f573b59b807b72a9ca1c1bd7fe8339
Ruksana <ruksana01official@gmail.com>
2024-02-08 09:47:07 +0600

    random

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-<<<<<<< HEAD
     baseURL: 'http://localhost:5000'
-=======
     // baseURL: 'https://ed-tech-server-six.vercel.app'
-    baseURL:'http://localhost:5000'
->>>>>>> 72f7b8775a9ea5e8be44b511e5d36c611bf5917e
+    // baseURL:'http://localhost:5000'
 })
 const useAxiosPublic = () => {
     return axiosPublic;
