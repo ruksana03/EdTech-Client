2a879bd5165673b9172efa3f8a99d7c5a9ab71f8
shaikot07 <saifulislamshaikat007@gmail.com>
2024-02-23 22:11:47 +0600

    report

--- src/Hooks/useGetAllPdf.jsx
+++ src/Hooks/useGetAllPdf.jsx
@@ -1,16 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
-import useAxiosPublic from './useAxiosPublic';
+// import { useQuery } from '@tanstack/react-query';
+// import axios from 'axios';
+// // import useAxiosPublic from './useAxiosPublic';
 
-const useGetAllPdf = () => {
-    const { data: allPdf = [], isLoading: loading, refetch } = useQuery({
-        queryKey: ["allPdf"],
-        queryFn: async () => {
-            const res = await useAxiosPublic.get("/get-pdf");
-            return res.data; // Corrected this line
-        }
-    });
-    console.log("this get all  pdf in axios", allPdf);
-    return [allPdf, loading, refetch]; // Changed return format to an array
-};
+// const useGetAllPdf = () => {
+//     // const axiosPublic=useAxiosPublic()
+//     const { data: allPdf = [], isLoading: loading, refetch } = useQuery({
+//         queryKey: ["allPdf"],
+//         queryFn: async () => {
+//             const res = await axios.get("/get-pdf");
+//             return res.data; // Corrected this line
+//         }
+//     });
+//     console.log("this get all  pdf in axios", allPdf);
+//     return [allPdf, loading, refetch]; // Changed return format to an array
+// };
 
-export default useGetAllPdf;
+// export default useGetAllPdf;
