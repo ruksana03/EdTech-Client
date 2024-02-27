38e1d08996ec1e8be262219a9b7c9ad1a0ff7fe6
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-02-27 19:47:24 +0600

    customize pdf in cerficate

--- src/page/dashboard/Student/certificates/pdf/PDF.jsx
+++ src/page/dashboard/Student/certificates/pdf/PDF.jsx
@@ -8,7 +8,7 @@ function PDF({anotherName}) {
         <Document>
             <Page size="A4">
                 <View style={{ position: 'relative', overflow: 'hidden' }}>
-                    <Text style={{ position: "absolute", top: '395px', right: 0, left: 110, zIndex: 1, fontSize: '60px', textTransform: 'capitalize', fontWeight: 'bold' }} >{anotherName}</Text>
+                    <Text style={{ position: "absolute", top: '395px', right: 0, left: 130, zIndex: 1, fontSize: '60px', textTransform: 'capitalize', fontWeight: 'bold' }} >{anotherName}</Text>
                     <Image src='https://i.ibb.co/N6gZBXk/certificate.png' style={{ width: '600', height: '800', zIndex: 3 }} />
                 </View>
             </Page>
