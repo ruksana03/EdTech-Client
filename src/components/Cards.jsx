a27528d808015ef0d17a6a3ce5e7cf9f33e8962e
nayon117 <hasibul.nayon1@gmail.com>
2024-02-27 19:47:18 +0600

    design card section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,38 +1,36 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from "react-router-dom";
-import { FaStar } from "react-icons/fa";
-import { LiaDirectionsSolid } from "react-icons/lia";
+
+// import { Link } from "react-router-dom";
+// import { FaStar } from "react-icons/fa";
+// import { LiaDirectionsSolid } from "react-icons/lia";
 
 const Cards = ({ item }) => {
-  const { _id, image, title, price } = item || {};
+  const { _id, image, title, price, duration, name } = item || {};
   return (
-    <div data-aos="flip-right"
-    data-aos-easing="ease-out-cubic"
-    data-aos-duration="2000"  className="relative mt-8 w-full max-w-xs overflow-hidden rounded-lg   bg-black   dark:border-white shadow-xl   mx-4 border border-gray-200  ">
-      <Link to={`/details/${_id}`}  >
-        <img className="h-60 rounded-t-lg object-cover" src={image} alt="Course image" />
-      </Link>
-      <span className="absolute top-0 left-0 w-28 translate-y-4 -translate-x-6 -rotate-45 bg-black text-center text-sm text-white">Hot</span>
-      <div className="mt-4 px-5 pb-5">
-        <a href="#">
-          <h5 className="text-xl font-semibold tracking-tight p__cormorant whitespace-nowrap">{title}</h5>
-        </a>
-        <div className="mt-2.5 mb-5 flex items-center">
-          <span className="mr-2 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
-          {[...Array(5)].map((_, index) => (
-            <FaStar key={index} className="h-5 w-5 text-yellow-300" />
-          ))}
-        </div>
-        <div className="flex items-center justify-between">
-          <p>
-            <span className="text-xl font-semibold p__opensans">${price}</span>
-            <span className="text-sm p__opensans line-through">$99</span>
+     <div data-aos="flip-right"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000" className="product-card rounded-md shadow-xl overflow-hidden z-[100] relative cursor-pointer snap-start shrink-0 py-8 px-6 flex flex-col items-center justify-center gap-6 transition-all duration-300 group  bg-black mx-4  border border-gray-200 ">
+       
+      <div className="w-[180px] aspect-square relative z-20 after:absolute after:h-1 after:w-full after:opacity-0 after:bg-[#7b956a] after:top-8 after:left-0 after:group-hover:opacity-100 after:translate-x-1/2 after:translate-y-1/2 after:-z-20 after:group-hover:w-full after:transition-all after:duration-300 after:group-hover:origin-right after:group-hover:-translate-x-1/2 group-hover:translate-x-[80%] transition-all duration-300">
+        <img src={image} alt="" className="h-48" />
+        <div className="tooltips absolute top-0 left-0 -translate-x-[150%] p-2 flex flex-col items-start gap-10 transition-all duration-300 group-hover:-translate-x-full">
+          <p className=" font-semibold font-alt text-white  uppercase group-hover:delay-500 transition-all opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
+            ${price}
           </p>
-          <Link to={`/details/${_id}`}   className="flex items-center rounded-md btn-style  ">
-          <LiaDirectionsSolid  className="text-lg"/>
-           <span className="ml-2">More</span>
-          </Link>
+          <ul className="flex flex-col items-start gap-2">
+            <li className="inline-flex gap-2 items-center justify-center group-hover:delay-200 transition-all opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
+              <p className="text-lg font-semibold font-alt text-white">{title}</p>
+            </li>
+            <li className="inline-flex gap-2 items-center justify-center group-hover:delay-300 transition-all opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
+              <p className="text-lg font-semibold font-alt text-white overflow-hidden whitespace-nowrap max-w-[160px]">{duration} months course</p>
+            </li>
+            <li className="inline-flex gap-2 items-center justify-center group-hover:delay-400 transition-all opacity-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500">
+              <p className="text-lg font-semibold font-alt text-white">{name}</p>
+            </li>
+            <Link to={`/details/${_id}`}><button className="btn btn-xs bg-[#F5EFDB]">Details</button></Link>
+          </ul>
         </div>
       </div>
     </div>
