9e863f0eea697ba994eeaea9c11c66e11ae443c2
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-04 17:22:19 +0600

    add and update some routine data

--- src/Hooks/useStudentRoutine.jsx
+++ src/Hooks/useStudentRoutine.jsx
@@ -7,7 +7,6 @@ const useStudentRoutine = () => {
     const user = useSelector(state => state.data.user.user);
     const [currentForRoutine, setCurrentForRoutine] = useState([])
     const axiosPublic = useAxiosPublic();
-    // const [newRoutine,setNewRoutine] = useState([]);
 
     useEffect(() => {
         if (user && user.email) {
@@ -23,8 +22,7 @@ const useStudentRoutine = () => {
     const specifcRoutine2 = currentForRoutine[1]?.title.toString();
     const specifcRoutine3 = currentForRoutine[2]?.title.toString();
     const specifcRoutine4 = currentForRoutine[3]?.title.toString();
-    // console.log("student routine--->",specifcRoutine1,",",specifcRoutine2,",",specifcRoutine3,",",specifcRoutine4);
-
+ 
 
     useEffect(() => {
         if (user && user.email) {
@@ -46,11 +44,10 @@ const useStudentRoutine = () => {
             const res3 = await axiosPublic.get(`/rutines-for?forCourses=${specifcRoutine3}`);
             const res4 = await axiosPublic.get(`/rutines-for?forCourses=${specifcRoutine4}`);
             const addRoutine = [...res1.data, ...res2.data, ...res3.data,...res4.data];
-        //   console.log('student routine--->', addRoutine);
             return addRoutine;
         }
     })
-    // console.log('student routine--->', newRoutine);
+    
     return [events, refetch];
 };
 
--- src/page/dashboard/Student/rutine/SRoutine.jsx
+++ src/page/dashboard/Student/rutine/SRoutine.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useRef, useState } from 'react';
 import FullCalendar from '@fullcalendar/react'
@@ -7,19 +8,19 @@ import interactionPlugin from '@fullcalendar/interaction'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment } from 'react';
 import './SRoutine.css'
-import useStudentRoutine from "../../../../Hooks/useStudentRoutine";
 
-const SRoutine = () => {
-    const [events, refetch] = useStudentRoutine();
-    console.log('new apdate---->',events);
+// eslint-disable-next-line react/prop-types
+const SRoutine = ({ events }) => {
+    // const [events, refetch] = useStudentRoutine();
+    // console.log('new apdate---->', events);
     const calendarRef = useRef(null);
     let [isOpen, setIsOpen] = useState(false);
     const [currentEvent, setCurrentEvent] = useState(null)
-    refetch();
+    // refetch();
     function closeModal() {
         setIsOpen(false)
     }
-    
+
     function handleDeleteClick(data) {
         setIsOpen(true)
         const currentData = data.event._def.extendedProps;
@@ -30,62 +31,72 @@ const SRoutine = () => {
 
     return (
         <section className='w-full h-screen mx-auto p-5'>
-            <div className='flex items-center justify-center gap-5 w-full'>
-                <button className='btn-style'>Class Routine</button>
-                <button className='btn-style'>Common Routine</button>
-                <button className='btn-style'>Program Routine</button>
-            </div>
-            <div className='w-[90%] mx-auto flex items-center justify-between p-5 flex-col '>
-                <div className='w-full'>
-                    <div className='relative z-0 w-full text-white'>
-                        <FullCalendar
-                            ref={calendarRef}
-                            events={events}
-                            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                            initialView="dayGridMonth"
-                            height={'80vh'}
-                            nowIndicator={true}
-                            droppable={true}
-                            eventClick={(data) => handleDeleteClick(data)}
-                            headerToolbar={{
-                                start: "today prev, next",
-                                center: 'title',
-                                end: "dayGridMonth,timeGridWeek,timeGridDay"
-                            }}
-                        />
+            <div>
+                {/* <div className='flex items-center justify-center gap-5 w-full'>
+                    {
+                       events?.length >= 2 && events?.map(routine => <div key={routine?._id}>
+                            <Link to={`/dashboard/routine/${routine?.forCourses?.slice(0, 7)}`}>
+                                <button className='btn-style'>{routine?.forCourses}</button>
+                            </Link>
+
+                        </div>)
+                    }
+
+                </div> */}
+                <div className='w-full lg:w-[90%] mx-auto flex items-center justify-between p-5 flex-col '>
+                    <div className='w-full'>
+                        <div className='relative z-0 w-full text-white'>
+                            <FullCalendar
+                                ref={calendarRef}
+                                events={events}
+                                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                                initialView="dayGridMonth"
+                                height={'80vh'}
+                                nowIndicator={true}
+                                droppable={true}
+                                eventClick={(data) => handleDeleteClick(data)}
+                                headerToolbar={{
+                                    start: "today prev, next",
+                                    center: 'title',
+                                    end: "dayGridMonth,timeGridWeek,timeGridDay"
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <>
-                <Transition appear show={isOpen} as={Fragment}>
-                    <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                        <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-                            <div className="fixed inset-0 bg-black/25" />
-                        </Transition.Child>
-                        <div className="fixed inset-0 overflow-y-auto">
-                            <div className="flex min-h-full items-center justify-center p-4 text-center p__cormorant">
-                                <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
-                                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-black text-white border border-white p-6 text-left align-middle shadow-xl transition-all flex items-center justify-center flex-col">
-                                        <Dialog.Title as="h3" className="text-lg capitalize font-medium leading-6 text-gray-900">
-                                            {currentEvent?.title}
-                                        </Dialog.Title>
-                                        <h1>Start Date : {currentEvent?.start?.slice(0, 10)}</h1>
-                                        <h1>End Date : {currentEvent?.end?.slice(0, 10)}</h1>
-                                        <h1>Meet Link : <a href={currentEvent?.liveLink} className='font-bold underline' target='_blank' rel='noopener noreferrer'>Click Here</a></h1>
+                <>
+                    <Transition appear show={isOpen} as={Fragment}>
+                        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                            <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
+                                <div className="fixed inset-0 bg-black/25" />
+                            </Transition.Child>
+                            <div className="fixed inset-0 overflow-y-auto">
+                                <div className="flex min-h-full items-center justify-center p-4 text-center p__cormorant">
+                                    <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
+                                        <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-black text-white border border-white p-6 text-left align-middle shadow-xl transition-all flex items-center justify-center flex-col space-y-2">
+                                            <Dialog.Title as="h3" className="text-lg capitalize font-medium leading-6 text-gray-900">
+                                                {currentEvent?.title}
+                                            </Dialog.Title>
+                                            <h1>Start Date : {currentEvent?.start?.slice(0, 10)}</h1>
+                                            <h1>End Date : {currentEvent?.end?.slice(0, 10)}</h1>
+                                            <h1>Meet Link : <a href={currentEvent?.liveLink} className='font-bold underline' target='_blank' rel='noopener noreferrer'>Click Here</a></h1>
+                                            <h1>Course : {currentEvent?.forCourses}</h1>
 
 
-                                        <div className="mt-4">
-                                            <button type="button" className="px-5 py-2 bg-green-700 text-white hover:text-black rounded" onClick={closeModal}>
-                                                Close
-                                            </button>
-                                        </div>
-                                    </Dialog.Panel>
-                                </Transition.Child>
+                                            <div className="mt-4">
+                                                <button type="button" className="px-5 py-2 bg-green-700 text-white hover:text-black rounded" onClick={closeModal}>
+                                                    Close
+                                                </button>
+                                            </div>
+                                        </Dialog.Panel>
+                                    </Transition.Child>
+                                </div>
                             </div>
-                        </div>
-                    </Dialog>
-                </Transition>
-            </>
+                        </Dialog>
+                    </Transition>
+                </>
+            </div>
+          
         </section>
     )
 };
